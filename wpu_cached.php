ac8a0c64c183aa14c5217ac5cf5e6e248a93f649
Kevin <kevin@rocher-dev.com>
2021-11-07 21:36:41 +0100

    v 0.2.0
    wpu_cached__wpdb_get
    - add cache_key.
    - handle get_var & get_row.

--- wpu_cached.php
+++ wpu_cached.php
@@ -4,7 +4,7 @@
 Plugin Name: WPU Cached
 Description: Simple helpers to help you cache anything
 Plugin URI: https://github.com/WordPressUtilities/wpu_cached
-Version: 0.1.1
+Version: 0.2.0
 Author: Darklg
 Author URI: https://darklg.me/
 License: MIT License
@@ -20,17 +20,20 @@ License URI: http://opensource.org/licenses/MIT
  * @param  string  $type           Type of WPDB get helper
  * @param  string  $q              MySQL Query, already prepared
  * @param  int     $cache_duration Cache duration in seconds
+ * @param  string  $cache_key      Cache key
  * @return mixed                   false or array
  */
-function wpu_cached__wpdb_get($type, $q, $cache_duration = false) {
+function wpu_cached__wpdb_get($type, $q, $cache_duration = false, $cache_key = false) {
     global $wpdb;
 
     /* Default arguments */
     $cache_duration = $cache_duration ?: 60;
     $type = $type ?: 'col';
 
+    $cache_key = $cache_key ? $cache_key : md5($q);
+
     /* Cache key */
-    $cache_id = 'wpdb_get_' . $type . '_' . md5($q);
+    $cache_id = 'wpdb_get_' . $type . '_' . $cache_key;
 
     /* Test cache */
     $result = wp_cache_get($cache_id);
@@ -38,6 +41,12 @@ function wpu_cached__wpdb_get($type, $q, $cache_duration = false) {
     /* Get results if cache is not available */
     if ($result === false) {
         switch ($type) {
+        case 'var':
+            $result = $wpdb->get_var($q);
+            break;
+        case 'row':
+            $result = $wpdb->get_row($q);
+            break;
         case 'col':
             $result = $wpdb->get_col($q);
             break;
